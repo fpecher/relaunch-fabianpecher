642acc4eb48f26608c8be57d83a5691deac88965
Fabian <pecherfabian@gmail.com>
2025-01-08 22:48:07 +0100

    [TASK] Common | added new project, made the last 2 projects smaller in die overview

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 export default defineConfig({
     image: {
         service: passthroughImageService()
-    }
+    },
 });
--- src/scripts/glide.js
+++ src/scripts/glide.js
@@ -1,9 +1,15 @@
 import Glide from "@glidejs/glide";
 
-export function glideInit() {
-    new Glide('.glide').mount({
+export function glideInit(selector, options = {}) {
+    const defaultOptions = {
         type: 'carousel',
         perView: 1,
         gap: 10,
-    });
+    };
+
+    // Merge default options with user-provided options
+    const glideOptions = { ...defaultOptions, ...options };
+    const glide = new Glide(selector, glideOptions);
+
+    glide.mount();
 }
