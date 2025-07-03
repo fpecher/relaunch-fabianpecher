672e5e3f4f6ab45d74ed21f4d32ebb960b77306c
Fabian <pecherfabian@gmail.com>
2025-07-03 09:36:38 +0200

    [TASK] Timeline | excluded work experience data from astro-file

---
+++ src/data/about/timeline-work-experience.ts
@@ -0,0 +1,66 @@
+export const workExperienceItems = [
+    {
+        additionalClass: "",
+        icon: "arrow-right",
+        date: "Mai 2022 – Januar 2025",
+        title: "Web Developer bei der JoussenKarliczek GmbH, Schorndorf",
+        description: "Entwicklung und Betreuung statischer und dynamischer Webseiten, stetige Weiterentwicklung des firmeneigenen WordPress-Themes.",
+        aos: "fade-up"
+      },
+      {
+        additionalClass: "",
+        icon: "arrow-right",
+        date: "August 2021 – April 2022",
+        title: "Marketing Assistant bei der ACTIV Werbetechnik GmbH, Kernen im Remstal",
+        description: "Betreuung der Socialmedia-Kanäle (einschließlich Gestaltung benötigter Grafiken), Design und Realisierung diverser Landingpages, Gestaltung verschiedener Printprodukte (z.B. Datenblätter, Flyer, Informationsmappen).",
+        aos: "fade-up"
+      },
+      {
+        additionalClass: "",
+        icon: "arrow-right",
+        date: "Februar 2018 – Juli 2021",
+        title: "Web Developer bei der SDV Werbestudio GmbH & Co. KG, Calw",
+        description: "Gestaltung, Entwicklung und Betreuung statischer und dynamischer Webseiten, Kundenkontakt und -betreuung.",
+        aos: "fade-up"
+      },
+      {
+        additionalClass: "",
+        icon: "arrow-right",
+        date: "April 2017 – 2022",
+        title: "Geschäftsführender Gesellschafter bei der World of Beachsports GbR, Tiefenbronn",
+        description: "Entwicklung Corporate Design, Entwicklung verschiedener Web-Applikationen, Gestaltung verschiedener Printprodukte (wie zum Beispiel Flyer, Plakate und Dropflags), Betreuung der Socialmedia-Kanäle (einschließlich Gestaltung benötigter Grafiken).",
+        aos: "fade-up"
+      },
+      {
+        additionalClass: "",
+        icon: "arrow-right",
+        date: "August 2016 – Januar 2018",
+        title: "Junior Software Developer bei der UDG United Digital Group GmbH, Ludwigsburg",
+        description: "Frontendseitige Entwicklung modulbasierter Code-Elemente und Erarbeiten einer Pattern-Library in einem größerem Projektteam.",
+        aos: "fade-up"
+      },
+      {
+        additionalClass: "",
+        icon: "arrow-right",
+        date: "November 2014 – Juli 2016",
+        title: "Frontend Developer bei der VoxelAir GmbH, Heimsheim",
+        description: "Entwicklung und Betreuung statischer und dynamischer Webseiten inklusive Kundenkontakt und -betreuung.",
+        aos: "fade-up"
+      },
+      {
+        additionalClass: "",
+        icon: "arrow-right",
+        date: "Juli 2014 – November 2014",
+        title: "Web Developer bei der SDV Werbestudio GmbH & Co. KG, Calw",
+        description: "Gestaltung, Entwicklung und Betreuung statischer und dynamischer Webseiten inklusive Kundenkontakt und -betreuung.",
+        aos: "fade-up"
+      },
+      {
+        additionalClass: "",
+        icon: "arrow-right",
+        date: "2009 – 2014",
+        title: "Selbstständige Tätigkeit bei der Kreativ-Agentur „co-op design“, Tiefenbronn",
+        description: "Gestaltung, Entwicklung und Betreuung statischer und dynamischer Webseiten. Realisierung verschiedener Print-Produkte (zum Beispiel Flyer, Plakate, Dropflags).",
+        aos: "fade-up"
+      }
+    ];
