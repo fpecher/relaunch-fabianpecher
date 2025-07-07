e1dd0380019e4772f70ec68665a0c0d52bb212b9
Fabian <pecherfabian@gmail.com>
2025-07-07 20:09:15 +0200

    [TASK] Common | some additional changes

--- src/data/about/timeline-work-experience.ts
+++ src/data/about/timeline-work-experience.ts
@@ -1,66 +1,74 @@
 export const workExperienceItems = [
-    {
-        additionalClass: "",
-        icon: "arrow-right",
-        date: "Mai 2022 – Januar 2025",
-        title: "Web Developer bei der JoussenKarliczek GmbH, Schorndorf",
-        description: "Entwicklung und Betreuung statischer und dynamischer Webseiten, stetige Weiterentwicklung des firmeneigenen WordPress-Themes.",
-        aos: "fade-up"
-      },
-      {
-        additionalClass: "",
-        icon: "arrow-right",
-        date: "August 2021 – April 2022",
-        title: "Marketing Assistant bei der ACTIV Werbetechnik GmbH, Kernen im Remstal",
-        description: "Betreuung der Socialmedia-Kanäle (einschließlich Gestaltung benötigter Grafiken), Design und Realisierung diverser Landingpages, Gestaltung verschiedener Printprodukte (z.B. Datenblätter, Flyer, Informationsmappen).",
-        aos: "fade-up"
-      },
-      {
-        additionalClass: "",
-        icon: "arrow-right",
-        date: "Februar 2018 – Juli 2021",
-        title: "Web Developer bei der SDV Werbestudio GmbH & Co. KG, Calw",
-        description: "Gestaltung, Entwicklung und Betreuung statischer und dynamischer Webseiten, Kundenkontakt und -betreuung.",
-        aos: "fade-up"
-      },
-      {
-        additionalClass: "",
-        icon: "arrow-right",
-        date: "April 2017 – 2022",
-        title: "Geschäftsführender Gesellschafter bei der World of Beachsports GbR, Tiefenbronn",
-        description: "Entwicklung Corporate Design, Entwicklung verschiedener Web-Applikationen, Gestaltung verschiedener Printprodukte (wie zum Beispiel Flyer, Plakate und Dropflags), Betreuung der Socialmedia-Kanäle (einschließlich Gestaltung benötigter Grafiken).",
-        aos: "fade-up"
-      },
-      {
-        additionalClass: "",
-        icon: "arrow-right",
-        date: "August 2016 – Januar 2018",
-        title: "Junior Software Developer bei der UDG United Digital Group GmbH, Ludwigsburg",
-        description: "Frontendseitige Entwicklung modulbasierter Code-Elemente und Erarbeiten einer Pattern-Library in einem größerem Projektteam.",
-        aos: "fade-up"
-      },
-      {
-        additionalClass: "",
-        icon: "arrow-right",
-        date: "November 2014 – Juli 2016",
-        title: "Frontend Developer bei der VoxelAir GmbH, Heimsheim",
-        description: "Entwicklung und Betreuung statischer und dynamischer Webseiten inklusive Kundenkontakt und -betreuung.",
-        aos: "fade-up"
-      },
-      {
-        additionalClass: "",
-        icon: "arrow-right",
-        date: "Juli 2014 – November 2014",
-        title: "Web Developer bei der SDV Werbestudio GmbH & Co. KG, Calw",
-        description: "Gestaltung, Entwicklung und Betreuung statischer und dynamischer Webseiten inklusive Kundenkontakt und -betreuung.",
-        aos: "fade-up"
-      },
-      {
-        additionalClass: "",
-        icon: "arrow-right",
-        date: "2009 – 2014",
-        title: "Selbstständige Tätigkeit bei der Kreativ-Agentur „co-op design“, Tiefenbronn",
-        description: "Gestaltung, Entwicklung und Betreuung statischer und dynamischer Webseiten. Realisierung verschiedener Print-Produkte (zum Beispiel Flyer, Plakate, Dropflags).",
-        aos: "fade-up"
-      }
-    ];
+  {
+    additionalClass: "",
+    icon: "arrow-right",
+    date: "Mai 2022 – Januar 2025",
+    employer: "JoussenKarliczek GmbH, Schorndorf",
+    title: "Web Developer",
+    description: "Entwicklung und Betreuung statischer und dynamischer Webseiten, stetige Weiterentwicklung des firmeneigenen WordPress-Themes.",
+    aos: "fade-up"
+  },
+  {
+    additionalClass: "",
+    icon: "arrow-right",
+    date: "August 2021 – April 2022",
+    employer: "ACTIV Werbetechnik GmbH, Kernen im Remstal",
+    title: "Marketing Assistant",
+    description: "Betreuung der Socialmedia-Kanäle (einschließlich Gestaltung benötigter Grafiken), Design und Realisierung diverser Landingpages, Gestaltung verschiedener Printprodukte (z.B. Datenblätter, Flyer, Informationsmappen).",
+    aos: "fade-up"
+  },
+  {
+    additionalClass: "",
+    icon: "arrow-right",
+    date: "Februar 2018 – Juli 2021",
+    employer: "SDV Werbestudio GmbH & Co. KG, Calw",
+    title: "Web Developer",
+    description: "Gestaltung, Entwicklung und Betreuung statischer und dynamischer Webseiten, Kundenkontakt und -betreuung.",
+    aos: "fade-up"
+  },
+  {
+    additionalClass: "",
+    icon: "arrow-right",
+    date: "April 2017 – 2022",
+    employer: "World of Beachsports GbR, Tiefenbronn",
+    title: "Geschäftsführender Gesellschafter",
+    description: "Entwicklung Corporate Design, Entwicklung verschiedener Web-Applikationen, Gestaltung verschiedener Printprodukte (wie zum Beispiel Flyer, Plakate und Dropflags), Betreuung der Socialmedia-Kanäle (einschließlich Gestaltung benötigter Grafiken).",
+    aos: "fade-up"
+  },
+  {
+    additionalClass: "",
+    icon: "arrow-right",
+    date: "August 2016 – Januar 2018",
+    employer: "UDG United Digital Group GmbH, Ludwigsburg",
+    title: "Junior Software Developer",
+    description: "Frontendseitige Entwicklung modulbasierter Code-Elemente und Erarbeiten einer Pattern-Library in einem größerem Projektteam.",
+    aos: "fade-up"
+  },
+  {
+    additionalClass: "",
+    icon: "arrow-right",
+    date: "November 2014 – Juli 2016",
+    employer: "VoxelAir GmbH, Heimsheim",
+    title: "Frontend Developer",
+    description: "Entwicklung und Betreuung statischer und dynamischer Webseiten inklusive Kundenkontakt und -betreuung.",
+    aos: "fade-up"
+  },
+  {
+    additionalClass: "",
+    icon: "arrow-right",
+    date: "Juli 2014 – November 2014",
+    employer: "SDV Werbestudio GmbH & Co. KG, Calw",
+    title: "Web Developer",
+    description: "Gestaltung, Entwicklung und Betreuung statischer und dynamischer Webseiten inklusive Kundenkontakt und -betreuung.",
+    aos: "fade-up"
+  },
+  {
+    additionalClass: "",
+    icon: "arrow-right",
+    date: "2009 – 2014",
+    employer: "co-op design, Tiefenbronn",
+    title: "Selbstständige Tätigkeit",
+    description: "Gestaltung, Entwicklung und Betreuung statischer und dynamischer Webseiten. Realisierung verschiedener Print-Produkte (zum Beispiel Flyer, Plakate, Dropflags).",
+    aos: "fade-up"
+  }
+];
---
+++ src/data/services/my-services.ts
@@ -0,0 +1,31 @@
+export const frontendItems = [
+    "HTML 5 & CSS 3",
+    "jQuery & Vanilla JS",
+    "BEM",
+    "SCSS",
+    "GSAP animation library",
+    "Github/Gitlab",
+    "WordPress",
+    "Typo3, Contao",
+    "Laravel"
+];
+
+export const uiSkills = [
+    "Responsive Design",
+    "Interaction Design",
+    "User Interface Design",
+    "User Experience Thinking",
+    "Accessibility",
+];
+
+export const teamBuilding = [
+    "Kommunikation",
+    "Empathie",
+    "Problemlöser",
+    "Offenheit",
+    "Zuverlässigkeit",
+    "Teamarbeit",
+    "Lächeln",
+    "",
+    "… und vieles mehr",
+];
