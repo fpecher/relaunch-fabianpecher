723ea69e5b61d253d0bcf7b7ce094e0d3600d13f
Fabian <pecherfabian@gmail.com>
2025-07-20 14:52:00 +0200

    Common Stuff | some changes and cleanups

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,10 @@
 // @ts-check
 import { defineConfig, passthroughImageService } from 'astro/config';
-import matomo from '@jop-software/astro-matomo';
 
 // https://astro.build/config
 export default defineConfig({
   image: {
       service: passthroughImageService()
   },
-  site: "https://fabianpecher.de",
-  integrations: [
-    matomo({
-      baseUrl: "https://matomo.fpecher.de/",
-      siteId: 1
-    }),
-  ],
 });
 
